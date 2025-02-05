cb908ba4737d475ce2cdb3cc1e8967770c253b87
DEV TRIVEDI <126101124+devtrivedi407@users.noreply.github.com>
2025-02-04 23:11:24 -0500

    Delete JS directory

--- JS/about.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // ✅ Set Static Impact Stats
-    document.getElementById("people-fed").textContent = 3;
-    document.getElementById("co2-reduced").textContent = 2500;
-    document.getElementById("money-saved").textContent = "$600";
-});
